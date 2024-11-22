5970788dd5edd8798d2010c948c086c81864474f
smitkakadiya20101991 <smitkakadiya@live.com>
2024-11-22 11:55:26 +0530

    main,customtextfirld,loginscreen

---
+++ lib/constants/textformfield/custom_textformfield.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:prachi_desktop_app/utils/colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+TextFormField customTextformfield(
+    String labelText, TextEditingController? controller, bool active) {
+  return TextFormField(
+    controller: controller,
+    decoration: InputDecoration(
+        contentPadding: EdgeInsets.only(left: 15),
+        suffixIcon: active
+            ? Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  "assets/svg/eye-off.svg",
+                  height: 15,
+                  width: 15,
+                  fit: BoxFit.cover,
+                ),
+              )
+            : Container(
+                height: 15,
+                width: 15,
+              ),
+        labelText: labelText,
+        labelStyle: TextStyle(color: appColors.textGreyColor),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: appColors.greyColor),
+          borderRadius: BorderRadius.circular(2),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: appColors.greyColor),
+          borderRadius: BorderRadius.circular(2),
+        ),
+        disabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: appColors.greyColor),
+          borderRadius: BorderRadius.circular(2),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: appColors.greyColor),
+          borderRadius: BorderRadius.circular(2),
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(color: appColors.greyColor),
+          borderRadius: BorderRadius.circular(2),
+        )),
+  );
+}
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  Color blueColor = const Color(0xFF0D5785);
+  Color blueColor = const Color(0xFF1877F2);
   Color whiteColor = Colors.white;
   Color blackColor = Color(0xFF313131);
   Color skyBlueColor = const Color(0xFFE7F6FF);
-  Color greyColor = const Color(0xFF6B6B6B);
+  Color greyColor = const Color(0xFF79747E);
   Color lightGreyColor = const Color(0xFFC1C1C1);
   Color textGreyColor = const Color(0xFF797979);
 }
